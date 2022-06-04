ded4205dfef170d683f5c178f828494f6a96a452
Omphile2002 <106697856+Omphile2002@users.noreply.github.com>
2022-06-04 17:21:55 +0200

    Search bar was just added and upload option.

--- app/src/main/java/com/example/ecommerce2/MainActivity.java
+++ app/src/main/java/com/example/ecommerce2/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.ecommerce2;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,7 +20,9 @@ import com.example.ecommerce2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     private ActivityMainBinding binding;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +38,38 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_buying, R.id.navigation_selling, R.id.navigation_signout)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        toolbar=findViewById(R.id.myToolBar);
+        setSupportActionBar(toolbar);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+
+        MenuItem.OnActionExpandListener onActionExpandListener=new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Toast.makeText(MainActivity.this, "Search is Expanded", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Toast.makeText(MainActivity.this, "Search is Collapse", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+        };
+
+        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        //searchView= (SearchView) menu.findItem(R.id.search).getActionView();
+        //searchView.setQueryHint("Search Item here...");
+
+        return true;
+    }
+
 
 }
